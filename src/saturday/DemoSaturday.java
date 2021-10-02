f81ac9af07ab5fc7ce8f47b76ac4940b45abb294
Yousuf Zia <Yousuf Zia@10.0.0.180>
2021-10-02 10:16:49 -0400

    My commit....

---
+++ src/saturday/DemoSaturday.java
@@ -0,0 +1,9 @@
+package saturday;
+
+public class DemoSaturday {
+	public static void main(String[] args) {
+		
+		System.out.println("Saturday Demo");
+	}
+
+}
---
+++ src/saturday/DemoSunday.java
@@ -0,0 +1,9 @@
+package saturday;
+
+public class DemoSunday {
+	public static void main(String[] args) {
+		
+		System.out.println("Demo Sunday");
+	}
+
+}
